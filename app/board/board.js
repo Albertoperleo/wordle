a0d6d9df044d8cbf836eec7488cc0d28c754545d
albperleo <albperleo@alum.us.es>
2022-02-28 17:24:48 +0100

    words list and changes in main.js

--- app/board/board.js
+++ app/board/board.js
@@ -1,16 +1,29 @@
-function draw(){
-    var cantidad=64;//document.getElementById('cantidad').value;
-    var alto=document.getElementById('alto').value;
-    var ancho=document.getElementById('ancho').value;
-    document.getElementById('cuadros').innerHTML='';
-    document.getElementById('cuadros').style.width = ((2 + parseInt(ancho)) * 8) + "px";
-    
-    for (var i=0;i<cantidad;i++){
-        if(Math.floor(i/8)%2 == i%2){
-            document.getElementById('cuadros').innerHTML+='<div id="cuadros" style="border:solid 1px;width:'+ ancho +'px;height:' + alto +'px;background-color:gray;float:left;"><div>';
-        }else{
-            document.getElementById('cuadros').innerHTML+='<div id="cuadros" style="border:solid 1px;width:'+ ancho +'px;height:' + alto +'px;background-color:black;float:left;"><div>';
-    
-        }
+
+export default class Board extends HTMLElement {
+    constructor(rand_word) {
+        this.rand_word = rand_word
+    }
+
+    //Métodos
+    draw(){
+        var cantidad=64;//document.getElementById('cantidad').value;
+        var alto=document.getElementById('alto').value;
+        var ancho=document.getElementById('ancho').value;
+        document.getElementById('cuadros').innerHTML='';
+        document.getElementById('cuadros').style.width = ((2 + parseInt(ancho)) * 8) + "px";
+        
+        for (var i=0;i<cantidad;i++){
+            if(Math.floor(i/8)%2 == i%2){
+                document.getElementById('cuadros').innerHTML+='<div id="cuadros" style="border:solid 1px;width:'+ ancho +'px;height:' + alto +'px;background-color:gray;float:left;"><div>';
+            }else{
+                document.getElementById('cuadros').innerHTML+='<div id="cuadros" style="border:solid 1px;width:'+ ancho +'px;height:' + alto +'px;background-color:black;float:left;"><div>';
+        
+            }
         }
     }
+}
+
+customElements.define("board", Board)
+
+
+
---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+import Board from "../board/board"
+
+function rand_word_searcher() {
+    
+}
+
+const tabla = new Board()
+
